72e6d0d104648cc8d14dbd6c570c8ca8c5996f59
MichelecFernandes <michelecristinafr@gmail.com>
2024-02-08 15:11:38 -0300

    Finalizando o exercicio 2

--- src/br/com/exercicio/main/Main.java
+++ src/br/com/exercicio/main/Main.java
@@ -13,7 +13,19 @@ public class Main {
 
         Produto produto = new Produto(valorLido);
         produto.obterValor();
-
         produto.mostraValor();
+
+
+        int valorC = 10;
+        double val = Double.parseDouble(String.valueOf(valorC));
+        System.out.printf("Valor real: %.0f %n", val);
+        System.out.println("Valor inteiro: " + valorC);
+
+
+        double valorD = 10;
+        int valorD_Covertido  = (int) valorD;
+        System.out.printf("Valor real: %.0f %n", valorD);
+        System.out.println("Valor inteiro: " + valorD_Covertido);
+
     }
 }
