f5f71d54f001659539e1d258e2d66f6eeb866508
MichelecFernandes <michelecristinafr@gmail.com>
2024-02-08 14:52:31 -0300

    Criando repositorio para o exercicio basico 02

--- src/br/com/exercicio/produto/Produto.java
+++ src/br/com/exercicio/produto/Produto.java
@@ -1,4 +1,24 @@
 package br.com.exercicio.produto;
 
 public class Produto {
+    private double valor;
+
+    public Produto(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void obterValor(){
+        System.out.println("O valor do produto é " + this.valor);
+
+    }
+    public void mostraValor(){
+        System.out.printf("Valor com casas decimais: %.2f %n", this.valor);
+        System.out.printf("Valor inteiro: %.0f %n", this.valor);
+    }
+
+
 }
